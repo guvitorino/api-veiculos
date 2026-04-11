982fe3c0ec7d9e932344afc8a5f07c558892a928
guvitorino <gu_vitorino@hotmail.com>
2026-04-10 22:04:15 -0300

    chore: initial project setup with maven and spring boot

---
+++ src/test/java/com/vitorino/api_veiculos/ApiVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorino.api_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
