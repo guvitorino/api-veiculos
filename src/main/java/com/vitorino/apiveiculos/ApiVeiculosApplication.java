dee61b404709c9052843a2bb4f2d5b22db8cbeb6
guvitorino <gu_vitorino@hotmail.com>
2026-04-11 11:20:36 -0300

    chore(Docs): setup to swagger ui

--- src/main/java/com/vitorino/apiveiculos/ApiVeiculosApplication.java
+++ src/main/java/com/vitorino/apiveiculos/ApiVeiculosApplication.java
@@ -1,4 +1,4 @@
-package com.vitorino.api_veiculos;
+package com.vitorino.apiveiculos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
