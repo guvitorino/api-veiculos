84aa1833323843818bdb0b45bbf7986103bfd39a
guvitorino <gu_vitorino@hotmail.com>
2026-04-14 14:23:32 -0300

    refactor(Redis): adjust config to use

---
+++ src/main/java/com/vitorino/apiveiculos/config/CacheConfig.java
@@ -0,0 +1,42 @@
+package com.vitorino.apiveiculos.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    private static final Duration DOLLAR_RATE_TTL = Duration.ofMinutes(10);
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        GenericToStringSerializer<BigDecimal> bigDecimalSerializer = new GenericToStringSerializer<>(BigDecimal.class);
+
+        RedisCacheConfiguration defaultConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer));
+
+        RedisCacheConfiguration dollarRateConfiguration = defaultConfiguration
+                .entryTtl(DOLLAR_RATE_TTL)
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(bigDecimalSerializer));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfiguration)
+                .withInitialCacheConfigurations(Map.of("dollarRate", dollarRateConfiguration))
+                .transactionAware()
+                .build();
+    }
+}
